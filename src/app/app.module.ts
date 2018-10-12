686efb5d699aa5f69843c2aa09f3ad95033ce604
Jeffrey Sanford <jeffrey@xorfox.com>
2018-10-12 14:19:43 -0600

    Initial commit for the NCO Evaluation reports rework.

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateModule } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { Keyboard } from '@ionic-native/keyboard';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Angulartics2Module } from 'angulartics2';
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
+
+import { environment } from '@env/environment';
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AboutModule } from './about/about.module';
+import { LoginModule } from './login/login.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
+    FormsModule,
+    HttpModule,
+    TranslateModule.forRoot(),
+    BrowserAnimationsModule,
+    MaterialModule,
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    AboutModule,
+    LoginModule,
+    Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [
+    Keyboard,
+    StatusBar,
+    SplashScreen
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/quote.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+const routes = {
+  quote: (c: RandomQuoteContext) => `/jokes/random?category=${c.category}`
+};
+
+export interface RandomQuoteContext {
+  // The quote's category: 'dev', 'explicit'...
+  category: string;
+}
+
+@Injectable()
+export class QuoteService {
+  constructor(private http: Http) {}
+
+  getRandomQuote(context: RandomQuoteContext): Observable<string> {
+    debugger;
+    return this.http.get(routes.quote(context)).pipe(
+      map((body: any) => body.value),
+      catchError(() => of('Error, could not load joke :-('))
+    );
+  }
+}
